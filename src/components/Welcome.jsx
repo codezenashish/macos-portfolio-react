9047589edbb253c15099e95cb2c341ab493ef4c2
codezenashish <ixtashish@gmail.com>
2025-12-08 22:44:11 +0530

    feat: update NavBar and Welcome components with text color for better visibility

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,9 +5,9 @@ const Welcome = () => {
 
   return (
     <section className='flex flex-col justify-center items-center min-h-screen' id='welcome'>
-      <p >Hay, I Ashish, a passionate developer.</p>
-      <h1>Portfolio</h1>
-      <div className="small-screen">
+      <p className='text-white font-semibold capitalize'>Hay, I Ashish, a passionate developer.</p>
+      <h1 className='text-white font-bold text-4xl'>Portfolio</h1>
+      <div className="sm:text-center text-white mt-4">
         <p>this portfolio is designed for desktop/tablet screens only.</p>
       </div>
     </section>
